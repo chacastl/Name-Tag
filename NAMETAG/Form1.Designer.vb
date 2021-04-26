dfb63c5153e4d6ca23f66340bc9cf1e1fe050ed9
chacastl <chacastl@iupui.edu>
2021-04-26 10:05:26 -0400

    Got the Setup exe working and Worked on UI

--- NAMETAG/Form1.Designer.vb
+++ NAMETAG/Form1.Designer.vb
@@ -39,22 +39,22 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(19, 265)
+        Me.TextBox1.Location = New System.Drawing.Point(59, 290)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(263, 20)
         Me.TextBox1.TabIndex = 0
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(19, 359)
+        Me.TextBox2.Location = New System.Drawing.Point(59, 379)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(263, 20)
         Me.TextBox2.TabIndex = 1
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(19, 448)
+        Me.RadioButton1.Location = New System.Drawing.Point(59, 476)
         Me.RadioButton1.Name = "RadioButton1"
         Me.RadioButton1.Size = New System.Drawing.Size(43, 17)
         Me.RadioButton1.TabIndex = 2
@@ -65,7 +65,7 @@ Partial Class Form1
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(19, 471)
+        Me.RadioButton2.Location = New System.Drawing.Point(59, 499)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(39, 17)
         Me.RadioButton2.TabIndex = 3
@@ -77,48 +77,48 @@ Partial Class Form1
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"", "Freshman", "Sophmore", "Junior", "Senior", "Graduate"})
-        Me.ComboBox1.Location = New System.Drawing.Point(19, 557)
+        Me.ComboBox1.Location = New System.Drawing.Point(59, 584)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(207, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(263, 21)
         Me.ComboBox1.TabIndex = 4
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("BentonSansCondensed", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 516)
+        Me.Label1.Font = New System.Drawing.Font("BentonSansCondensed", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(52, 533)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(71, 38)
+        Me.Label1.Size = New System.Drawing.Size(89, 48)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Year"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("BentonSansCondensed", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 407)
+        Me.Label2.Font = New System.Drawing.Font("BentonSansCondensed", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(51, 425)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(106, 38)
+        Me.Label2.Size = New System.Drawing.Size(132, 48)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "Alumni"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("BentonSansCondensed", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(12, 318)
+        Me.Label3.Font = New System.Drawing.Font("BentonSansCondensed", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(51, 328)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(90, 38)
+        Me.Label3.Size = New System.Drawing.Size(111, 48)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Major"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("BentonSansCondensed", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(12, 220)
+        Me.Label4.Font = New System.Drawing.Font("BentonSansCondensed", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(51, 239)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(155, 42)
+        Me.Label4.Size = New System.Drawing.Size(177, 48)
         Me.Label4.TabIndex = 8
         Me.Label4.Text = "Full Name"
         '
@@ -127,13 +127,13 @@ Partial Class Form1
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(106, 12)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(795, 185)
+        Me.PictureBox1.Size = New System.Drawing.Size(812, 205)
         Me.PictureBox1.TabIndex = 9
         Me.PictureBox1.TabStop = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(19, 619)
+        Me.Button1.Location = New System.Drawing.Point(59, 639)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(130, 70)
         Me.Button1.TabIndex = 10
@@ -144,7 +144,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1010, 722)
+        Me.ClientSize = New System.Drawing.Size(1096, 756)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label4)
--- NAMETAG/Form1.vb
+++ NAMETAG/Form1.vb
@@ -1,6 +1,26 @@
-﻿
+﻿Imports System.IO
+Imports System.Text
 Public Class Form1
 
+    Private Sub TextBox1_Click(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button1_Click(Nothing, Nothing)
+        End If
+
+    End Sub
+    Private Sub TextBox2_Click(sender As Object, e As KeyEventArgs) Handles TextBox2.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button1_Click(Nothing, Nothing)
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_Click(sender As Object, e As KeyEventArgs) Handles ComboBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button1_Click(Nothing, Nothing)
+        End If
+
+    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ' All Code was referenced from https://github.com/dymosoftware/DCD-SDK-Sample for printing from a Dymo printer
         Dim DymoPrint As DymoSDK.Implementations.DymoPrinter
@@ -22,17 +42,30 @@ Public Class Form1
         Else
             DymoSDKLabel.UpdateLabelObject(LabelTextObject3, "Alumni")
         End If
-        Dim file As System.IO.StreamWriter
-        file = My.Computer.FileSystem.OpenTextFileWriter("test.txt", True)
-        file.WriteLine(" Student Name: " + TextBox1.Text + " Major: " + TextBox2.Text + " Year: " + ComboBox1.Text)
-        file.Close()
 
+
+        Try
+            Dim filePath As String
+            filePath = System.IO.Path.Combine(
+               My.Computer.FileSystem.SpecialDirectories.MyDocuments, "Names.txt")
+            If (RadioButton2.Checked = True) Then
+                My.Computer.FileSystem.WriteAllText(filePath, "Name: " + TextBox1.Text + " Major: " + TextBox2.Text + " Year: " + ComboBox1.Text + vbNewLine, True)
+            Else
+                My.Computer.FileSystem.WriteAllText(filePath, "Name: " + TextBox1.Text + " Major: " + TextBox2.Text + " Year: Alumni" + vbNewLine, True)
+            End If
+        Catch fileException As Exception
+            Throw fileException
+        End Try
         DymoPrint.Instance.PrintLabel(DymoSDKLabel, "DYMO LabelWriter 450", 1)
         TextBox1.Clear()
         TextBox2.Clear()
         ComboBox1.SelectedIndex = 0
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        ComboBox1.Enabled = True
 
     End Sub
+
 
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
         ComboBox1.Enabled = True
@@ -41,4 +74,5 @@ Public Class Form1
     Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
         ComboBox1.Enabled = False
     End Sub
+
 End Class
